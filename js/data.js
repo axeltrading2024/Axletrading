c1bbf6cdd0689ed35f7ddf4d503c0d2930eb3d57
axeltrading2024 <eddy_wanderlust@163.com>
2026-09-01 22:20:56 +0800

    更新产品数据（后台一键发布）

--- js/data.js
+++ js/data.js
@@ -209,9 +209,9 @@ window.PRODUCTS = [
   {
     "id": "jbl-charge",
     "name": "JBL Charge 6",
-    "brand": "JBL",
-    "price": "$129",
-    "note": "Powerbank speaker",
+    "brand": "Oem",
+    "price": "€20",
+    "note": "Powerbank speaker-protable",
     "desc": "Room-filling sound plus a built-in powerbank to top up your phone on the go.",
     "collection": "speakers",
     "keywords": [
