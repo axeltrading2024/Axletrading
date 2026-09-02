03d53401e757fd854fff3e5c7f8bf819562081aa
axeltrading2024 <eddy_wanderlust@163.com>
2026-09-02 21:20:36 +0800

    更新产品数据（后台一键发布）

--- js/data.js
+++ js/data.js
@@ -520,7 +520,7 @@ window.PRODUCTS = [
       {
         "name": "rayban meta",
         "price": "",
-        "image": ""
+        "image": "img/prod/rayban-meta-rayban-meta.jpg"
       }
     ]
   },
