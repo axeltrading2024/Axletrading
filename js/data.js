f04c0c5d33dadd11e9c9563b8be6c131e4e3dd0c
axeltrading2024 <eddy_wanderlust@163.com>
2026-09-06 16:30:08 +0800

    更新产品数据（后台一键发布）

--- js/data.js
+++ js/data.js
@@ -2058,6 +2058,38 @@ window.PRODUCTS = [
     "note": "bluetooth 5.2-mini",
     "desc": "",
     "collection": "speakers"
+  },
+  {
+    "id": "g-3",
+    "name": "Gshock style",
+    "brand": "",
+    "price": "€18",
+    "note": "sport-passion-young",
+    "desc": "",
+    "collection": "watches",
+    "subcategory": "luxury",
+    "variants": [
+      {
+        "name": "GA 2100",
+        "price": "",
+        "image": "img/prod/g-3-ga-2100.jpg"
+      },
+      {
+        "name": "GA800",
+        "price": "",
+        "image": "img/prod/g-3-ga800.jpg"
+      },
+      {
+        "name": "GA110",
+        "price": "",
+        "image": "img/prod/g-3-ga110.jpg"
+      },
+      {
+        "name": "GA+BA couple",
+        "price": "",
+        "image": "img/prod/g-3-ga-ba-couple.jpg"
+      }
+    ]
   }
 ];
 
