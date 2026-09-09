e27c1251d1517e05a4fb74ff18cbd4c4a3b40c5d
axeltrading2024 <eddy_wanderlust@163.com>
2026-09-09 17:01:52 +0800

    更新产品数据（后台一键发布）

--- js/data.js
+++ js/data.js
@@ -898,38 +898,6 @@ window.PRODUCTS = [
     ]
   },
   {
-    "id": "g-3",
-    "name": "Gshock style",
-    "brand": "",
-    "price": "€18",
-    "note": "sport-passion-young",
-    "desc": "",
-    "collection": "watches",
-    "subcategory": "luxury",
-    "variants": [
-      {
-        "name": "GA 2100",
-        "price": "",
-        "image": "img/prod/g-3-ga-2100.jpg"
-      },
-      {
-        "name": "GA800",
-        "price": "",
-        "image": "img/prod/g-3-ga800.jpg"
-      },
-      {
-        "name": "GA110",
-        "price": "",
-        "image": "img/prod/g-3-ga110.jpg"
-      },
-      {
-        "name": "GA+BA couple",
-        "price": "",
-        "image": "img/prod/g-3-ga-ba-couple.jpg"
-      }
-    ]
-  },
-  {
     "id": "c-2",
     "name": "Cartier style",
     "brand": "",
@@ -983,6 +951,38 @@ window.PRODUCTS = [
         "name": "9",
         "price": "",
         "image": "img/prod/c-2-9.jpg"
+      }
+    ]
+  },
+  {
+    "id": "g-3",
+    "name": "Gshock style",
+    "brand": "",
+    "price": "€18",
+    "note": "sport-passion-young",
+    "desc": "",
+    "collection": "watches",
+    "subcategory": "luxury",
+    "variants": [
+      {
+        "name": "GA 2100",
+        "price": "",
+        "image": "img/prod/g-3-ga-2100.jpg"
+      },
+      {
+        "name": "GA800",
+        "price": "",
+        "image": "img/prod/g-3-ga800.jpg"
+      },
+      {
+        "name": "GA110",
+        "price": "",
+        "image": "img/prod/g-3-ga110.jpg"
+      },
+      {
+        "name": "GA+BA couple",
+        "price": "",
+        "image": "img/prod/g-3-ga-ba-couple.jpg"
       }
     ]
   },
