661b56d561ce7801c6a4bef8289320d24f3a7d83
axeltrading2024 <eddy_wanderlust@163.com>
2026-09-11 11:04:52 +0800

    更新产品数据（后台一键发布）

--- js/data.js
+++ js/data.js
@@ -1758,6 +1758,67 @@ window.PRODUCTS = [
     ]
   },
   {
+    "id": "r-3",
+    "name": "Rayban style sunglasses",
+    "brand": "Ray-Ban",
+    "price": "€35",
+    "note": "fashion-uvprotection",
+    "desc": "",
+    "collection": "sunglasses",
+    "subcategory": "luxury",
+    "keywords": [
+      "rayban",
+      "sunglasses"
+    ],
+    "variants": [
+      {
+        "name": "1",
+        "price": "",
+        "image": "img/prod/r-3-1.jpg"
+      },
+      {
+        "name": "2",
+        "price": "",
+        "image": "img/prod/r-3-2.jpg"
+      },
+      {
+        "name": "3",
+        "price": "",
+        "image": "img/prod/r-3-3.jpg"
+      },
+      {
+        "name": "4",
+        "price": "",
+        "image": "img/prod/r-3-4.jpg"
+      },
+      {
+        "name": "5",
+        "price": "",
+        "image": "img/prod/r-3-7.jpg"
+      },
+      {
+        "name": "6",
+        "price": "",
+        "image": "img/prod/r-3-8.jpg"
+      },
+      {
+        "name": "7",
+        "price": "",
+        "image": "img/prod/r-3-7.jpg"
+      },
+      {
+        "name": "8",
+        "price": "",
+        "image": "img/prod/r-3-8.jpg"
+      },
+      {
+        "name": "9",
+        "price": "",
+        "image": "img/prod/r-3-9.jpg"
+      }
+    ]
+  },
+  {
     "id": "l",
     "name": "LV style bags",
     "brand": "",
@@ -2650,67 +2711,6 @@ window.PRODUCTS = [
         "name": "10",
         "price": "",
         "image": "img/prod/l-3-10.jpg"
-      }
-    ]
-  },
-  {
-    "id": "r-3",
-    "name": "Rayban style sunglasses",
-    "brand": "Ray-Ban",
-    "price": "€35",
-    "note": "fashion-uvprotection",
-    "desc": "",
-    "collection": "sunglasses",
-    "subcategory": "luxury",
-    "keywords": [
-      "rayban",
-      "sunglasses"
-    ],
-    "variants": [
-      {
-        "name": "1",
-        "price": "",
-        "image": "img/prod/r-3-1.jpg"
-      },
-      {
-        "name": "2",
-        "price": "",
-        "image": "img/prod/r-3-2.jpg"
-      },
-      {
-        "name": "3",
-        "price": "",
-        "image": "img/prod/r-3-3.jpg"
-      },
-      {
-        "name": "4",
-        "price": "",
-        "image": "img/prod/r-3-4.jpg"
-      },
-      {
-        "name": "5",
-        "price": "",
-        "image": "img/prod/r-3-7.jpg"
-      },
-      {
-        "name": "6",
-        "price": "",
-        "image": "img/prod/r-3-8.jpg"
-      },
-      {
-        "name": "7",
-        "price": "",
-        "image": "img/prod/r-3-7.jpg"
-      },
-      {
-        "name": "8",
-        "price": "",
-        "image": "img/prod/r-3-8.jpg"
-      },
-      {
-        "name": "9",
-        "price": "",
-        "image": "img/prod/r-3-9.jpg"
       }
     ]
   },
